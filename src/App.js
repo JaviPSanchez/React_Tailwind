83c9052ab35111fc31717a55e07f0fa799ad2bce
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2022-02-06 16:06:59 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import styled from "styled-components";
+import React from "react";
+
+function App() {
+  return <Container>Hola</Container>;
+}
+
+const Container = styled.div`
+  display: inline-block;
+  background-color: red;
+  padding: 5rem;
+`;
+
+export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export const themeColor = "#27DBD8";
+export const darkThemeColor = "#0c4241";
+export const hoverEffect = `rgba(0, 0, 0, 0.2) 0px 10px 20px 4px`;
+export const cardShadow = `rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px`;
